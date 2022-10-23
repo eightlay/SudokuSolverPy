8b370554cf99fd8d2a7f74c57d4e2cd481bf70d6
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2022-10-23 20:43:29 +0400

    sudoku game settings

---
+++ src/settings.py
@@ -0,0 +1,18 @@
+_FIELD_W = 9
+"""Width of the field"""
+_FIELD_H = 9
+"""Height of the field"""
+_BLOCK_W = 3
+"""Width of the block"""
+_BLOCK_H = 3
+"""Height of the block"""
+_NUMBER_MIN = 1
+"""Minimal possible number"""
+_NUMBER_MAX = 9
+"""Maximal possible number"""
+_NUMBER_RANGE = range(_NUMBER_MIN, _NUMBER_MAX + 1)
+"""Numbers' range"""
+_NUMBER_STR_TEMPLATE = " {0} "
+"""Template string for number representation"""
+_NUMBER_STR_TEMPLATE_LEN = 3
+"""Length of the actual number representation"""
